154973bebb38a95f4163b52c46f50a968c9d7b26
Mariam Makboub <mmakboub@e3r4p8.1337.ma>
2023-07-14 23:48:52 +0100

    done

--- Inception-Docker-main/INCEPTION/src/requirement/wordpress/tools/script.sh
+++ Inception-Docker-main/INCEPTION/src/requirement/wordpress/tools/script.sh
@@ -1,8 +1,16 @@
- #!/bin/bash
-wp core download --allow-root --version=6.2.2
-wp  creat config set DB_NAME "wordpress" --allow-root
-wp config set DB_USER "mmakboub" --allow-root
-wp config set DB_PASSWORD "azerty321QWE" --allow-root
-wp config set DB_HOST "mariadb" --allow-root
-wp core install  --allow-root --url=$WP_URL --title=$WP_TITLE --admin_name=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL
-wp user --allow-root create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASS --role=author
+#!/bin/bash
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+	service php7.3-fpm start
+	chmod 777 /var/www/html/
+	chown -R www-data:www-data /var/www/html/
+	wp core download --allow-root --path=/var/www/html --allow-root
+	# wp config create --allow-root --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_PASSWORD} --dbhost="mariadb" --path=/var/www/html --allow-root --skip-check
+	# wp core install --allow-root --path=/var/www/html
+	# wp core install --allow-root --url=${DOMAIN_NAME} --title='inception' --admin_user=${ADMIN_NAME} --admin_password=${ADMIN_PASS} --admin_email=$ADMIN_EMAIL --allow-root --path=/var/www/html
+	# wp user create --allow-root ${WP_USER_NAME $WP_EMAIL} --user_pass=${WP_USER_PASS} --allow-root --path=/var/www/html
+fi
+while true; do
+	sleep 1000
+done
+exec "$@"
